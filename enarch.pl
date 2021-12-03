4f741c57dbc0a5a006be63cd042f5373347d3039
wbudic <vilijanac@gmail.com>
2021-12-03 11:09:56 +1100

    Major updates. New arguments, lot's of fixes.

--- enarch.pl
+++ enarch.pl
@@ -1,8 +1,10 @@
 #!/bin/perl
 use strict;
 use sigtrap qw/die normal-signals/;
+use File::stat;
 
-my ($gpgpass,$target,$name,$archive,$action,$postfix,$fuzzy,$showfull,$cocoon, $attach_letter, $vim, $nolog, $cocodb, $alias);
+my ($gpgpass,$target,$name,$archive,$action,$postfix,$fuzzy,$showfull,$cocoon, $attach_letter, $vim, $nolog, $cocodb, $alias, $add);
+my $xs_level=6;
 my $host   =`uname -n`;
 my $date   = `date "+%Y%m%d"`;
 my $ext    = 'tar.gz.enc';
@@ -14,28 +16,31 @@ my $DEBUG  = 0;
 my $COCOON_ATTACH_LETTER = 'cocoon_attached_letter.txt';
 my $BACKUP_START=`date '+%F %T'`;
 my $BACKUP_END;
+$ENV{HOME}='/home/will' if not $ENV{HOME};
 
 foreach my $a (@ARGV){
-    my $v = $a; $v =~ s/^-+.*=//;
-    print "[$a]->$v\n" if $DEBUG;
+    my $v = $a; $v =~ s/^-+.*[=:]//;
+    print "[$a] -> $v\n" if $DEBUG;
     if($a =~ m/~/g){print "Error: Directory substitution is not permited. Use full paths please!\n"; exit 2;}    
-    elsif($a =~ m/^-+.*(pass)/i)        {$gpgpass = gpgPassCodeCheck($a)}
+    elsif($a =~ m/^-+.*(pass)/i)       {$gpgpass = gpgPassCodeCheck($v);next}
     elsif($a =~ m/^-+.*(generate)/i)    {print "New gpg passcode: ", gpgPassCodeGenerate(), "\n";exit 1;}
     elsif($a =~ m/^-+.*(target)/i)      {$paths{$target} = $target if $target; $target = $v;}
     elsif($a =~ m/^-+.*(name)/i)        {$name = $v;}
     elsif($a =~ m/^-+.*(letter)/i)      {$attach_letter = $v;next}
     elsif($a =~ m/^-+.*(vim)/i)         {$attach_letter = $v;$vim = 1;next}
-    elsif($a =~ m/^-+.*(cocodb)/i)      {$action='ARCHIVE' if !$action; $cocoon=$v; $cocodb="cocoon.db";next}
-    elsif($a =~ m/^-+.*(add-to-db)/i)   {$alias=lc $v;next}
+    elsif($a =~ m/^-+.*(coco.*db)/i)    {$action='ARCHIVE' if !$action; $alias=$v; $cocodb = $ENV{HOME}."/.config/cocoon.db";next}
+    elsif($a =~ m/^-+.*(add-to-db)/i)   {$action='REGISTER'; $add=1;$alias=lc $v;next}
     elsif($a =~ m/^-+.*(cocoon)/i)      {$action='ARCHIVE' if !$action; $cocoon=$v;next}
     elsif($a =~ m/^-+.*(list)/i)        {$action='LIST'; $showfull=1 if $v eq 'full'}
     elsif($a =~ m/^-+.*(fuzzy)|(fzf)/i) {$action='LIST'; $fuzzy=1; $showfull=1 if $v eq 'full'}
-    elsif($a =~ m/^-+.*(restore)/i)     {$action='RESTORE'}
+    elsif($a =~ m/^-+.*(extract)/i)     {$action='RESTORE';next}    
+    elsif($a =~ m/^-+.*(restore)/i)     {$action='RESTORE';next}
     elsif($a =~ m/^-+h|\?/i)            {$action='HELP' if !$action; &printHelp;}
     elsif($a =~ m/^-+.*(postfix)/i)     {$postfix = $v; $postfix =0 if $postfix =~ /^of+/ || $postfix =~ /^0/}
     elsif($a =~ m/^-+.*(ex)/i)          {$exclds{"--exclude=$v"}=$v}    
-    elsif($a =~ m/^-+.*(cur)|(f)/i)     {$curr{"$v"}=$v}
+    elsif($a =~ m/^-+.*(cur)|(f)/i)     {$v=strip_trailing($v);$curr{"$v"}=$v}
     elsif($a =~ m/^-+.*(no-logging)/i)  {$nolog=1}
+    elsif($a =~ m/^-+.*(xs_level)/i)    {$xs_level=$v if $a ne $v; next}
     elsif($a !~ m/^-+/){                        
         if(-d $a){           
             if (!$target) {
@@ -48,20 +53,27 @@ foreach my $a (@ARGV){
                 $action='ARCHIVE';
                 $paths{$a}=$a;  next;
             }
-        }{
-            print "Error: Directory path specified is not a valid directory: [$a]\n"; exit 2;
+        }elsif(-f $a && $a =~ m/.*\.cocoon$/){
+                $target = $a if !$target
+        }else{
+            print "Error: Directory or path specified is not a valid directory: [$a]\n"; exit 2;
         }
-    }
-    elsif($a =~ m/^-+restore/i) {$action='RESTORE'}
+    }    
     else{
          print "Error: Don't understand argument: [$a]\n"; exit 2;
     }    
 }
+sub strip_trailing{
+    my $r = shift;
+    if($action eq 'RESTORE'){
+       $r=~ s/^\///g;
+    }    
+    return $r;
+}
 
-
-if($cocoon){
-   if($cocodb){exit 1 if not &cocoonDB}
-   &cocoon; exit 1;
+if($cocoon || $cocodb){
+   if($cocodb){&cocoonDB; exit 1 if not $cocoon}
+   &cocoon if $cocoon; exit 1;
 }
 elsif($action ne 'HELP'){
     if(!$target){
@@ -96,21 +108,24 @@ sub archive {
 }
 # An cocoon archive, must have specific cocoon password to which also an letter can be attached.
 sub cocoon {
-    if(!$name){
+    
+    if($action eq "ARCHIVE" && !$name){
         if($cocoon =~ m/.*-cocoon$/){
             print cocoonPassword($gpgpass)."\n";
             exit 0;
         }
-        print "Error: Cocoon name not specified!\n"; exit 2;
+        print "Error: Cocoon name not specified! action is $action\n"; 
+        exit 2;
+    }else{         
+        if($name){$archive = "$name.cocoon"}else{$archive = $target}
+        $gpgpass = $cocoon if not $gpgpass;  
     }
-    $archive = "$name.cocoon";
-    $cocoon  = $gpgpass if $cocoon =~ m/.*-cocoon$/g;
-    $cocoon  = cocoonPassword($cocoon);
     my $res;
+    $cocoon =cocoonPassword($gpgpass);
     if($action eq "LIST"){
         if ($showfull) {$showfull="-Jtv"}else{$showfull="-Jt"}
         if ($fuzzy){$fuzzy = "$showfull | fzf --multi --no-sort --sync"}else{$fuzzy = $showfull};
-           $res = system("gpg --no-verbose --decrypt --batch --passphrase $cocoon $archive 2>/dev/null | tar $fuzzy ");    
+           $res = system("gpg --no-verbose --decrypt --batch --passphrase $cocoon $archive 2>/dev/null | pv -N 'Obtaining Listing' | tar $fuzzy ");    
         if($res){
             print "Error: Failed to list archive: $archive. Cocoon password suplied: $cocoon\n";
         }else{
@@ -133,14 +148,14 @@ sub cocoon {
         }
     }
     elsif($action eq "RESTORE"){
-        my $files = join(' ', sort(keys %curr));
+        my $files = join ' ', map "'$_'", sort(keys %curr);        
         $res = system("gpg --decrypt --batch --passphrase $cocoon $archive | tar -Jxv --strip-components 2 $files");
         if($res){
             print "Error: Failed to restore archive: $archive. Cocoon password suplied: $cocoon\n";
         }else{
             print "Restored archive: $archive\n";
         }
-    }else{
+    }elsif($action eq "ARCHIVE"){
         if($attach_letter){
 
             if($attach_letter =~ /-letter$/ || $attach_letter =~ /-vim$/){
@@ -175,21 +190,41 @@ sub cocoon {
             }
         }
         print "Generating cocoon: $archive\n";$paths{$target} = $target;
-        $res = system("XZ_OPT=-9; tar -Jcvi ".join(' ', sort(keys %exclds))." ".
-                            join(' ', sort(keys %paths))." ".
-                            join(' ', sort(keys %curr)).
-                " | pv -N 'Cocooning' -t -b -e -r | gpg -c --no-symkey-cache --batch --passphrase $cocoon > $archive");
+        $res = "tar -cvi --use-compress-program=\'xz -"."$xs_level"."T3\'".join(' ', sort(keys %exclds))." ".
+                            join(' ', map "'$_'", sort(keys %paths))." ".
+                            join(' ', map "'$_'", sort(keys %curr)).
+                " | pv -N 'Cocooning' -t -b -e -r | gpg -c --no-symkey-cache --batch --passphrase $cocoon > $archive";
+        print $res, "\n\n";
+        $res = system($res);
+
         if($res){
-            print "Action: $action failed! Err: $!\n";     
-        }else{
-            `rm $attach_letter` if $attach_letter eq $COCOON_ATTACH_LETTER;
-            print "Action: $action Finished succesfully!\n";
-            print "Cocoon passcode: $cocoon\n";            
+            print "Action: $action failed! Err: $!\n"; 
+            exit 0;    
+        }else{            
+             my $stats = stat($archive);
+            `rm $attach_letter` if $attach_letter eq $COCOON_ATTACH_LETTER;            
+            
+            print  "Action: $action Finished succesfully!\n";            
+            print  "Cocoon passcode: $cocoon\n";            
+            printf "Cocoon archive: $archive size: %s date: %s \n", human($stats->size),scalar localtime $stats->mtime;
             logToCnf($cocoon, $archive);
             &printArchivingTook;
         }
     }
 }
+
+sub human {
+        my $x = shift;               
+        my $s=" B   KiB MiB GiB TiB EiB PiB YiB ZiB";
+        while ($x>=1024 && length($s)>1){
+               $x/=1024; $s=substr($s,4)
+        }
+        my $xf=($s==" B  ")?"~%d ":"%8.2f";
+        $s=substr($s,1,3);        
+        return sprintf( "$xf%s", $x, $s)
+}
+      
+
 sub printArchivingTook {
     $BACKUP_END=`date '+%F %T'`;
     my $out =  `dateutils.ddiff -f "%H hours and %M minutes %S seconds." "$BACKUP_START" "$BACKUP_END"`;
@@ -323,32 +358,41 @@ sub cocoonDB {
          close $fh;
     }    
 
-    if($action eq 'LIST'){
+    if($action eq 'LIST' &&not $target ){
         foreach (sort keys %data) {
             print $_,'=', $data{$_}, "\n";
         }
         exit 2;
     }
     
-    if($alias){ #if $alias is set we are adding/modifying the db.     
+    if($alias){ #if $alias is set we are adding/modifying the db.         
+        if(!$add && $action eq 'RESTORE'){
+            $cocoon = $data{$alias};
+            return undef if $cocoon;            
+        }
            my %v =  map { $_ => 1 } values %data;
-           $data{$alias} = &gpgPassCodeGenerate for exists($v{$_});
+           if($gpgpass){ #We force the value
+               $data{$alias} = $gpgpass; 
+           }else{#or generate new one
+               $data{$alias} = &gpgPassCodeGenerate for exists($v{$_});           
+           }
            open(my $fh, '>', $cocodb) or die "Couldn't write to $cocodb, $!";
            foreach my $k(sort keys %data) {
             print $fh $k,'=', $data{$k}, "\n";
            }
            close $fh;
-           $cocoon=$data{$alias} if $cocoon;
+           print "Added $alias $cocoon to [$cocodb]\n" if $DEBUG;
+           $cocoon=$data{$alias};           
     }elsif($cocoon){ #the alias|email is assigned to the $cocoon variable instead of an passcode.
-       if(not $cocoon=$data{$cocoon}){
-           print "Error alias|email of -> $cocoon not found on system!";
+       if(not $data{$cocoon}){
+           print "Error alias|email of -> '$cocoon' not found on system!\n";
            exit 0;
-       }
-       if(not $name){
-          print $cocoon;
-          exit 1;
-       } 
-    }#not last assigned is returned in perl, which is the $cocoon value, a pass code.
+       }else{
+        $cocoon= $data{$cocoon}
+       }       
+       print $target, " <-- $action gpgpass[", $cocoon,"]\n";  
+       exit 1 if not $name and not $target       
+    }    
 }
 
 
@@ -381,6 +425,7 @@ Options:
 -vim            - Open vim to enter the letter or message for the archive.
 -no-logging     - Archive action is logged automatically to ~/.config/enarch.log, use this option to dissable this.
 -cocodb={email} - Interact with cocoondb for listing, adding, obtaining a stored coocon pass code (see more about bellow).
+-xz-level={-6}  - XZ_OPT compression level to use (from 0..9).
 
 -h/? - This help file.
 
@@ -429,6 +474,20 @@ Format:
    $ enarch --cocodb=alias|email
 
 --
+
+-- Full Examples of working with a single file cocoon via the cocodb. alias enarch=~/dev/enarch.pl 
+$ enarch --cocodb=SomeEmail@protonmail.com -name=attachment -f='/home/user1/Documents/resume.pdf'
+$ enarch --cocodb=SomeEmail@protonmail.com --list=full --target=attachment.coon
+$ enarch --cocodb=SomeEmail@protonmail.com --restore --target=attachment.coon
+    -- If emailed the passcode you register and use alias john (the emailer generated code) rather then his email.
+    $ enarch --cocodb --gpgpass="1W-EA-WA-48-HC-DM-C2-EA" --add-to-db=john
+    -- Finally extract the whole.
+    $ enarch --cocodb:john --restore --name=attachment
+    -- or
+    $ enarch --cocodb:john --restore --target=attachment.cocoon
+    -- To extract by specific file.
+    $ enarch --cocodb:john --restore --name=attachment -f="/home/user1/Documents/resume.pdf"
+
 Notice:
  
  - Argument assignment is smart switching, '--'arg '-'arg and '--pass' with '--gpgpass' are the same.
@@ -439,7 +498,7 @@ Notice:
  - `enarch --gpgpass=xxx... --cocoon`, will dump out for you, the required travel pass.
  
 Requirments:
-openpgp, fzf
+sudo apt install openpgp fzf pv
 
 --------------------------------------------------------------------------------------------------------------
 # This file originated from https://github.com/wbudic/B_L_R_via_sshfs
